4b23f6ac294b4957cd87b94a32a7603691897ff2
Vlad <belousvladislav7@gmail.com>
2025-05-19 01:56:52 +0300

    Create login page

--- Lab2/pzpi-22-4-bilous-vladyslav-lab2/app/build.gradle.kts
+++ Lab2/pzpi-22-4-bilous-vladyslav-lab2/app/build.gradle.kts
@@ -39,10 +39,9 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
