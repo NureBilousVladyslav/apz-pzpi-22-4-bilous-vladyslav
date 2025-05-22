fa67168786d41ed8238f5c7e7d46966f1af178d0
Vlad <belousvladislav7@gmail.com>
2025-05-22 16:48:45 +0300

    add frontend

---
+++ Lab3/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import Navigation from './components/navigation/navigation';
+import NotFound from './pages/notFound/notFound';
+import Customers from './pages/customers';
+import Admins from './pages/admins';
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useContext } from 'react';
+import { useCookies } from 'react-cookie';
+import i18n from './configs/locale';
+import { AuthContext } from './contexts/authContext';
+import { LoginForm } from './components/forms/login';
+
+function App() {
+  const [cookies, setCookie] = useCookies(['language']);
+  const { isAuthenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    const currentLanguage = cookies.language || 'en';
+    i18n.changeLanguage(currentLanguage);
+  }, [cookies.language]);
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+    setCookie('language', language, { path: '/', maxAge: 24 * 60 * 60 });
+  };
+
+  return (
+    <div className="app">
+      {isAuthenticated && <Navigation changeLanguage={changeLanguage} />}
+      <Routes>
+        <Route path='/' element={<LoginForm changeLanguage={changeLanguage} />} />
+        <Route path='/customers' element={<Customers />} />
+        <Route path='/admins' element={<Admins />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
