8c78bc79c3358a1d9cba27ef78ba7f0e5f6b7aed
Vlad <belousvladislav7@gmail.com>
2025-06-01 20:23:42 +0300

    repair link backend

--- Lab3/src/configs/api.js
+++ Lab3/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://tire-presure-backend-dpg3ejcpgpbnfmeh.northeurope-01.azurewebsites.net',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
